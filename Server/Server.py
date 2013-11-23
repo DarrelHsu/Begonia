88f1826052e9fcb6b0f28644caa11e52481f7d43
DarrelHsu <darui.xdr@alibaba-inc.com>
2013-11-23 11:58:57 +0800

    整理引用

--- Server/Server.py
+++ Server/Server.py
@@ -1,11 +1,11 @@
 import web
 import re
 DEBUG = False 
-URLS={
+__urls__={
     "GET" : {} ,
     "POST" : {}  
 }
-CACHES={
+__caches__={
     "GET" : {} ,
     "POST" : {}  
 }
@@ -23,18 +23,18 @@ class requestHandler :
         if DEBUG is True :
             if path == '/shutdown' :
                 exit()
-        if path in CACHES[ method ] :
-            handlerMethod = CACHES[ method ] [ path ]
-        elif path in URLS[ method ]  :
-            handlerMethod = URLS[ method ] [ path ]
+        if path in __caches__[ method ] :
+            handlerMethod = __caches__[ method ] [ path ]
+        elif path in __urls__[ method ]  :
+            handlerMethod = __urls__[ method ] [ path ]
         else :
-            for key in URLS :
+            for key in __urls__ :
                 reg = re.compile( "^%s$" % key )
                 matchs = reg.match( path )
                 if matchs is not None :
-                    handlerMethod = URLS[ method ][ key ] 
+                    handlerMethod = __urls__[ method ][ key ] 
                     break 
-            CACHES[ method ][ path ] = handlerMethod 
+            __caches__[ method ][ path ] = handlerMethod 
         if handlerMethod is None :
             return web.notfound()
         else : 
@@ -44,30 +44,30 @@ class requestHandler :
         return self.handler( );
     def GET( self , var = None , args = None ) :
         return self.handler( );
-def add_request_handler( path , fn , method = None ) :
+def __add_request_handler( path , fn , method = None ) :
     if method is None :
         method = "REQUEST"
     else :
         method = method.upper(  ) 
         if method != 'GET' and method != 'POST' :
             method = 'GET'
-        URLS[ method ] [ path ]= fn
-def request_method( path , method = "REQUEST" ):
+        __urls__[ method ] [ path ]= fn
+def __request_method( path , method = "REQUEST" ):
     def _( *args , **kws ): 
         fn = args[0]
-        add_request_handler( path , fn , method )
+        __add_request_handler( path , fn , method )
     return _
 
 def GET( path ) :
     def _( *args , **kws ): 
         fn = args[0]
-        add_request_handler( path , fn , "GET" )
+        __add_request_handler( path , fn , "GET" )
     return _
 
 def POST( path ) :
     def _( *args , **kws ): 
         fn = args[0]
-        add_request_handler( path , fn , "POST" )
+        __add_request_handler( path , fn , "POST" )
     return _
 
 class Server :
@@ -82,7 +82,9 @@ if __name__ == '__main__' :
         @GET("/fuck")
         def fuck( ) :
             return web.input()
-            return "fuck"
+        @POST("/fuck")
+        def fuckpost() :
+            return web.data()
     DEBUG = True
     server = myServer()
     server.start()
--- Server/__init__.py
+++ Server/__init__.py
@@ -1,1 +1,3 @@
-
+from  Server import POST
+from  Server import GET
+from  Server import Server as server
